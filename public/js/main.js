b2460110106c0272aa4ddf1f1a8606f45a0b1aee
Ankitkj1999 <57802410+Ankitkj1999@users.noreply.github.com>
2020-09-24 20:31:11 +0530

    added search and rest

--- public/js/main.js
+++ public/js/main.js
@@ -460,3 +460,20 @@ const renderCertification = ( data, id) => {
 
 Certification.innerHTML += html; 
 
+// Prevent closing from click inside dropdown
+$(document).on('click', '.dropdown-menu', function (e) {
+  e.stopPropagation();
+});
+
+// make it as accordion for smaller screens
+if ($(window).width() < 992) {
+  $('.dropdown-menu a').click(function(e){
+	e.preventDefault();
+	  if($(this).next('.submenu').length){
+		$(this).next('.submenu').toggle();
+	  }
+	  $('.dropdown').on('hide.bs.dropdown', function () {
+	 $(this).find('.submenu').hide();
+  })
+  });
+}
